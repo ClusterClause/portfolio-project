bdf68fb0fc4e68c895457ee6629642bb2df68b4b
Tim Bigelow-Martin <tim.MT_B@yahoo.com>
2020-01-07 15:08:40 -0800

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'nymphsblitzquickvexdwarfjog',
+        'USER':'user',
+        'PASSWORD':'password',
         'HOST':'localhost',
         'PORT':'5432',
     }
